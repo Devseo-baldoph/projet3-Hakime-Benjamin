34c408752d2b2565897090405478b9ed8eabff85
hakime-AI <hakimeattoumani.simplon@gmail.com>
2022-01-10 11:23:28 +0100

    maj init.php 1.03

--- php/init.php
+++ php/init.php
@@ -1,9 +1,12 @@
 <?php 
-session_start();
+// session_start();
 class ConnexionDb{
+    
     public $codb;
     public $sql; 
-    public function __construct(){
+    public $print;
+    
+    public function __construct(){ // s'active automatiquement en debut de class
         try{
             $codb = new PDO("mysql:host=localhost;dbname=gite", "root", "",[
             PDO::ATTR_ERRMODE=> PDO::ERRMODE_EXCEPTION,
@@ -19,9 +22,11 @@ class ConnexionDb{
         }
     }
 
-    public function __destruct(){
+    public function __destruct(){ // s'active automatiquement en fin de class
         try{
-            $this->codb->exec($this->sql);
+            $query=$this->codb->query($this->sql);
+            $this -> print=$query->fetchall();
+            // echo "<pre>",print_r($this -> print),"</pre>" ;
             $this->codb = null;
         }
         catch(PDOException $e)
@@ -30,30 +35,114 @@ class ConnexionDb{
             echo "Message d'erreur : " .$e->getMessage(). "<br />";   
         }
     }
+    
+    // select
+        public function SelectTb(){
+            $this ->sql = "SHOW TABLES";
+        }
+    
+        public function SelectCl($tbname){
+            $this->sql = "SHOW COLUMNS FROM $tbname";
+            echo $this->sql;
+        }
+        
+        public function SelectRow($tbname,$colonne,$value){
+            $this->sql = "SELECT * FROM $tbname WHERE $colonne='$value'";
+        }
 
+    // création
     public function CreateTb($tbname){
         $this ->sql = "CREATE TABLE IF NOT EXISTS $tbname(
             id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY)";
     }
 
     public function CreateCl($tbname,$colonne,$type){
-            $this->sql = "ALTER TABLE $tbname ADD $colonne $type";
+        $this->sql = "ALTER TABLE $tbname ADD $colonne $type";
     }
     
-    public function CreateVl($tbname,$colonne,$value){
-        $this->sql = "INSERT INTO $tbname($colonne)
-            VALUES ('$value')";
+    public function CreateRow($tbname,$colonne,$value){
+        $newColonne='';
+        $newValue='';
+        for($i=0;$i<count($colonne);$i++){
+            $newColonne .= $colonne[$i].",";
+        }
+        for($i=0;$i<count($value);$i++){
+            $newValue .= "'". $value[$i]."',";
+        }
+        $newColonne = substr($newColonne, 0, -1);
+        $newValue = substr($newValue, 0, -1);
+        echo $newColonne ,"<br>";
+        $this->sql = "INSERT INTO $tbname($newColonne)
+            VALUES ($newValue)";
     }
+    
+    //Suppression
+    public function SupTb($tbname){
+        $this ->sql = "DROP TABLE $tbname";
+    }
+    
+    public function SupCl($tbname,$colonne){
+        $this->sql = "ALTER TABLE $tbname DROP $colonne";
+    }
+
+    // public function SupRow($tbname,$colonne,$value){
+    //     $this->sql = "UPDATE $tbname
+    //     SET $colonne = ''
+    //     WHERE $colonne = $value";
+    // }
 }
 
 // ---------------------------------------------------------------
 
-// $ConnexionDb = new ConnexionDb; // A utiliser pour creer
-// $ConnexionDb -> CreateTb("test");  // une Table
+$SelectDb = new ConnexionDb; // A utiliser pour supprimer
+
+$SelectDb -> SelectTb();  // une Table
+// $SelectDb -> print;
+echo "<pre>",print_r($SelectDb -> print),"</pre>" ;
+
+// $SelectDb -> SelectCl("wen");  // une Colonne
+
+// $SelectDb -> SelectRow("gites","Nom","chambre");  // une Valeur
+// ---------------------------------------------------------------
 
 // $ConnexionDb = new ConnexionDb; // A utiliser pour creer
-// $ConnexionDb -> CreateCl("test","nom","VARCHAR(40)");  // une Colonne
+// $ConnexionDb -> CreateTb("wen");  // une Table
 
 // $ConnexionDb = new ConnexionDb; // A utiliser pour creer
-// $ConnexionDb -> CreateVl("test","nom","hakime");  // une Valeur
+// $ConnexionDb -> CreateCl("wen","age","VARCHAR(40)");  // une Colonne
+
+// $colonne= array(
+// "Nom",
+// "Description",
+// "Photos",
+// "Couchages	",
+// "SallesDeBain",
+// "Emplacement	",
+// "Prix",
+// "Disponibilite"
+// );
+// $value= array(
+//     "cave",
+//     "50m2",
+//     "x",
+//     "2",
+//     "2",
+//     "en Bas",
+//     "350",
+//     "Dispo"
+//     );
+
+// $ConnexionDb = new ConnexionDb; // A utiliser pour creer
+// $ConnexionDb -> CreateRow("gites",$colonne,$value);  // une Ligne
+
+// ---------------------------------------------------------------
+
+// $ConnexionDb = new ConnexionDb; // A utiliser pour supprimer
+// $ConnexionDb -> SupTb("wen");  // une Table
+
+// $ConnexionDb = new ConnexionDb; // A utiliser pour supprimer
+// $ConnexionDb -> SupCl("wen","age","VARCHAR(40)");  // une Colonne
+
+// $ConnexionDb = new ConnexionDb; // A utiliser pour supprimer
+// $ConnexionDb -> SupVl("gites","Nom",null);  // une Valeur
 ?>
